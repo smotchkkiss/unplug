b0ca5762ea2c77e81ec0b9193d4616616ca099f6
Emanuel Tannert <post@etannert.de>
2018-10-01 15:31:53 +0200

    more refactoring; split up the unplug\unplug function

--- unplug.php
+++ unplug.php
@@ -24,29 +24,8 @@ if (!defined('ABSPATH')) {
 }
 
 
-// make sure UNPLUG_CACHE is defined,
-// so we don't have to check that everytime
 if (!defined('UNPLUG_CACHE')) {
     define('UNPLUG_CACHE', false);
-}
-
-
-/**
- * Gets the current url, but without protocol/host
- * By Giuseppe Mazzapica @gmazzap as published on
- * https://roots.io/routing-wp-requests/
- *
- * @returns String url
- */
-function get_current_url() {
-
-    $current_url = trim(esc_url_raw(add_query_arg(array())), '/');
-    $home_path = trim(parse_url(home_url(), PHP_URL_PATH), '/');
-    if ($home_path && strpos($current_url, $home_path) === 0) {
-        $current_url = trim(substr($current_url, strlen($home_path)), '/');
-    }
-
-    return $current_url;
 }
 
 
@@ -81,8 +60,10 @@ function _get_default_router() {
         $router->_use([
             'request' => function(&$context) {
                 $site_url = get_site_url();
+                while (substr($site_url, -1) === '/') {
+                    $site_url = substr($site_url, 0, -1);
+                }
                 $context['site_url'] = $site_url;
-                // TODO sure that site_url never has a trailing slash?
                 $context['current_url'] = $site_url.$context['path'];
                 $context['theme_url'] = get_template_directory_uri();
                 $context['site_title'] = get_bloginfo();
@@ -113,64 +94,73 @@ function _get_default_router() {
  */
 function unplug($options=array()) {
 
-    // check if this is an admin-panel
-    // request, and if not, prevent wordpress from parsing
-    // the url and running a query based on it.
-    // ---
-    // if the path starts with admin or login,
-    // which are two convenient wordpress redirects, we don't
-    // want to prevent the parsing, either. Same goes for paths
-    // starting with wp-content so a '*'-route won't falsely
-    // catch uploads or static theme assets.
-    $path = parse_url(get_current_url(), PHP_URL_PATH);
-    $allowed = !preg_match('/^(admin|login|wp-content)/', $path);
-    $allowed = $allowed && (!is_admin() || (defined('DOING_AJAX') && DOING_AJAX));
-
-    if ($allowed) {
-        add_action('do_parse_request', function($do_parse, $wp) {
-            $wp->query_vars = array();
-            remove_action('template_redirect', 'redirect_canonical');
-            return FALSE;
-        }, 30, 2);
+    if (is_frontend_request()) {
+        prevent_wp_default_query();
     }
 
-    // if caching is on, make sure to empty the cache on
-    // save_post and set a few constants so the router
-    // knows whether we want to cache or not
     if (UNPLUG_CACHE) {
-
-        if (isset($options['cache_dir'])) {
-
-            define('UNPLUG_CACHE_DIR', $options['cache_dir']);
-
-        } else {
-
-            define('UNPLUG_CACHE_DIR', __DIR__ . '/_unplug_cache');
-        }
-
-        $after_save_post = function() use ($options) {
-
-            // this function will only be called if caching
-            // is on, so it's safe to assume that
-            // UNPLUG_CACHE_DIR will be set
-            Cache::get_instance()->flush();
-
-            if (isset($options['on_save_post'])) {
-                $options['on_save_post']($cache);
-            }
-        };
-
-        add_action('save_post', $after_save_post, 20);
-
-        if (is_acf_active()) {
-            add_action('acf/save_post', $after_save_post, 20);
-        }
+        set_cache_dir($options);
+        flush_cache_on_save_post($options);
     }
 
-    // hide the sample permalink on the edit post page when unplug
-    // is in use, because the whole point of unplug is to implement
-    // your own routing, so what wordpress thinks what the url of a
-    // post is will often be wrong.
+    hide_wp_sample_permalink();
+}
+
+
+function is_frontend_request() {
+    $path = _get_default_router()->get_request_path();
+    $is_wp_admin_path = preg_match('/^(admin|login|wp-content)/', $path);
+    if ($is_wp_admin_path) {
+        return FALSE;
+    }
+
+    // TODO why exactly do we handle requests when DOING_AJAX is
+    // defined and true?
+    // maybe the next two lines could be replaced with just
+    // if (is_admin()) {
+    $doing_ajax = defined('DOING_AJAX') && DOING_AJAX;
+    if (is_admin() && !$doing_ajax) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+function prevent_wp_default_query() {
+    add_action('do_parse_request', function($do_parse, $wp) {
+        $wp->query_vars = array();
+        remove_action('template_redirect', 'redirect_canonical');
+        return FALSE;
+    }, 30, 2);
+}
+
+
+function set_cache_dir($options) {
+    if (isset($options['cache_dir'])) {
+        define('UNPLUG_CACHE_DIR', $options['cache_dir']);
+    } else {
+        define('UNPLUG_CACHE_DIR', __DIR__ . '/_unplug_cache');
+    }
+}
+
+
+function flush_cache_on_save_post($options) {
+    $after_save_post = function() use ($options) {
+        Cache::get_instance()->flush();
+        if (isset($options['on_save_post'])) {
+            $options['on_save_post']($cache);
+        }
+    };
+
+    add_action('save_post', $after_save_post, 20);
+    if (is_acf_active()) {
+        add_action('acf/save_post', $after_save_post, 20);
+    }
+}
+
+
+function hide_wp_sample_permalink() {
     add_filter('get_sample_permalink_html', function() {
         return '';
     });
