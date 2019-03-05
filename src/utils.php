b72fb1c0575c0ae078605ca22597db3a3057a6b6
Emanuel Tannert <post@etannert.de>
2019-03-05 15:31:56 +0100

    only introduce is_acf_active if ABSPATH is defined

--- src/utils.php
+++ src/utils.php
@@ -3,9 +3,9 @@
 namespace Em4nl\Unplug;
 
 
-include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+if (defined('ABSPATH') && !function_exists('Em4nl\Unplug\is_acf_active')) {
+    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
-if (!function_exists('Em4nl\Unplug\is_acf_active')) {
     function is_acf_active() {
         return is_plugin_active('advanced-custom-fields-pro/acf.php')
             || is_plugin_active('advanced-custom-fields/acf.php');
