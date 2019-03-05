af34f72dd7b8f278446231dc67b96750fa112530
Emanuel Tannert <post@etannert.de>
2019-03-05 16:48:40 +0100

    always define is_acf_active; return false if no better answer

--- src/utils.php
+++ src/utils.php
@@ -3,11 +3,14 @@
 namespace Em4nl\Unplug;
 
 
-if (defined('ABSPATH') && !function_exists('Em4nl\Unplug\is_acf_active')) {
-    include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-
+if (!function_exists('Em4nl\Unplug\is_acf_active')) {
     function is_acf_active() {
-        return is_plugin_active('advanced-custom-fields-pro/acf.php')
-            || is_plugin_active('advanced-custom-fields/acf.php');
+        if (defined('ABSPATH')) {
+            include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+
+            return is_plugin_active('advanced-custom-fields-pro/acf.php')
+                || is_plugin_active('advanced-custom-fields/acf.php');
+        }
+        return FALSE;
     }
 }
